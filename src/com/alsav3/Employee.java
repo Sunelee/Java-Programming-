cd0f76da0ce0e2fe8a4c9160860272bb646ea702
AslaV3 <teamallghostanon@gmail.com>
2021-07-16 11:07:47 +0200

    Classes

---
+++ src/com/alsav3/Employee.java
@@ -0,0 +1,35 @@
+package com.alsav3;
+
+public class Employee {
+    //attributes
+    private String name;
+    private int age;
+    private String position;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+}
+
+
--- src/com/alsav3/Main.java
+++ src/com/alsav3/Main.java
@@ -1,39 +1,23 @@
 package com.alsav3;
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-	    Loops loops = new Loops(); //loops class [object]
-        Functions func = new Functions();
 
-        //functions
-//        System.out.println("Please enter num 1");
-//        int num1 = scanner.nextInt();
-//        System.out.println("Please enter num 2");
-//        int num2 = scanner.nextInt();
-//
-//        int sum = func.divideNumbers(num1,num2);
-//        if (sum == 0){
-//            System.out.println("One number is zero");
-//        }else{
-//            System.out.println(sum);
-//        }
-        System.out.println("Please enter name");
-        String name = scanner.nextLine();
-        String saidName = func.sayMyName(name);
-        System.out.println("Your name is: "+saidName);
+    Employee employee = new Employee();
+    Employee employee1 = new Employee();
 
+    employee.setName("Musa Dlamini");
+    employee.setPosition("CEO");
+    employee.setAge(28);
 
-//    loops.forLoop(7, 90);
-//        loops.forLoop(90, 4);
-//        loops.forLoop(0, 500);
+    employee1.setAge(40);
+
+    System.out.println(employee1.getAge());
+
+    System.out.println("Name: \t"+employee.getName()+"\tPosition: "+employee.getPosition()+"\tAge: "+employee.getAge());
 
     }
 
-    void oIt(){
-
-    }
 }
