2baae6b1d0b24e488ff59899c0d731fad1e5daed
AslaV3 <teamallghostanon@gmail.com>
2021-07-16 10:40:03 +0200

    functions

--- src/com/alsav3/Functions.java
+++ src/com/alsav3/Functions.java
@@ -1,6 +1,29 @@
 package com.alsav3;
 
 public class Functions {
+    //access modifier, return type, name, parameter list
+    public int addNumbers(int num1, int num2){
+        if (num1 == 0 && num2 == 0){
+            return 0;
+        }
+       return num1 + num2;
+    }
 
+    public int multiplyNumbers(int num1, int num2){
+        if (num1 == 0 && num2 == 0){
+            return 0;
+        }
+        return num1 * num2;
+    }
 
+    public int divideNumbers(int num1, int num2){
+        if (num1 == 0){
+            return 0;
+        }
+        return num1 / num2;
+    }
+
+    public String sayMyName(String name){
+        return name;
+    }
 }
--- src/com/alsav3/Main.java
+++ src/com/alsav3/Main.java
@@ -1,13 +1,35 @@
 package com.alsav3;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	Loops loops = new Loops(); //loops class [object]
+        Scanner scanner = new Scanner(System.in);
+	    Loops loops = new Loops(); //loops class [object]
+        Functions func = new Functions();
 
-    loops.forLoop(7, 90);
-        loops.forLoop(90, 4);
-        loops.forLoop(0, 500);
+        //functions
+//        System.out.println("Please enter num 1");
+//        int num1 = scanner.nextInt();
+//        System.out.println("Please enter num 2");
+//        int num2 = scanner.nextInt();
+//
+//        int sum = func.divideNumbers(num1,num2);
+//        if (sum == 0){
+//            System.out.println("One number is zero");
+//        }else{
+//            System.out.println(sum);
+//        }
+        System.out.println("Please enter name");
+        String name = scanner.nextLine();
+        String saidName = func.sayMyName(name);
+        System.out.println("Your name is: "+saidName);
+
+
+//    loops.forLoop(7, 90);
+//        loops.forLoop(90, 4);
+//        loops.forLoop(0, 500);
 
     }
 
